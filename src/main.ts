3fc6d4928953f272c2576e00b3c0c7fe891f397d
hiennghia11 <lamhiennghia1110@gmail.com>
2024-03-25 11:56:03 +0700

    add reset-css, pinia store, vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import "./styles/global.css";
+import "./styles/reset.css";
+import router from "./router";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+const pinia = createPinia();
+createApp(App).use(pinia).use(router).mount("#app");
---
+++ src/router.ts
@@ -0,0 +1,10 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [{ path: "/", component: () => import("./views/HomeView.vue") }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
